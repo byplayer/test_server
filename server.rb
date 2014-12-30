a8402f240e9bdd11d9879eae8dccd6f4a6aafe2e
byplayer <byplayer100@gmail.com>
2014-12-30 17:31:09 +0800

    add debug server

---
+++ server.rb
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+require 'webrick'
+
+srv = WEBrick::HTTPServer.new({ :DocumentRoot => './',
+                                :BindAddress => '127.0.0.1',
+                                :AccessLog => [
+                                               [File.open('access.log', 'a+'),
+                                                WEBrick::AccessLog::CLF]],
+                                :Port => 20080})
+
+# SIGINT を捕捉する。
+Signal.trap('INT') do
+  # 捕捉した場合、シャットダウンする。
+  srv.shutdown
+end
+
+srv.mount_proc('/') do |req, _res|
+  File.open('detail.log', 'a+') do |f|
+    f.puts Time.now.strftime('%Y.%m.%d %H:%M:%S:') +
+      req.path + ' ' + req.query_string
+    f.puts req.body
+    f.puts req.header.inspect
+  end
+end
+
+srv.start
